72281fab35dd690142ecaf0ca8b9c16886a140e3
elikandid <edogbey64@gmail.com>
2024-06-28 09:29:25 +0000

    React Router

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,8 +1,8 @@
 import welcomeImage from "../assets/images/welcome-image.png"
 export default function Welcome() {
     return(
-        <div>
-            <h3> Savoury & Sweet</h3>
+        <div className="bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.5)),url('./assets/images/welcome-bg-image.png')] bg-cover h-screen bg-center flex flex-col place-items-center">
+            <h3 className="text-white text-center text-4xl"> Savoury & <br />Sweet</h3>
             <img src={welcomeImage} alt="" />
         </div>
     );
